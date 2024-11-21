e61a4270e54243f99769b0a5040d8e02eb4d8c90
salihrizakartal <salihkartal53@gmail.com>
2024-11-21 16:17:13 +0300

    add

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    //generic constraint
+    //class referans tip
+    //IEntity olabilir veya IEntity implemente eden bir nesne
+    //new() : new lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter =null);  //Datayı filtreleyip getirmrk için(Delege)
+        T Get(Expression<Func<T, bool>> filter);          //Tek detay getirmek için
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
